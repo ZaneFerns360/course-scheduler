0609cf9efeb27d2803339cb288efe4153b7e87e4
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-07-25 17:29:52 +0530

    commit 1.01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/Navbar";
-import Blogs from "@/components/blogs";
+
+
 
 export default function Home() {
   return (
     <main className="flex-col min-h-screen items-center justify-center">
-     <Navbar/> 
-      <Blogs/>
+ 
+ 
     </main>
   );
 }
