3da9c69d5ee361161aaaae7c877e1e979059f183
BeatCo <chrislopes3667@gmail.com>
2024-08-02 08:52:03 +0530

    fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,11 +5,13 @@ const Hero = () => {
   return (
     <div className="-z-10 fixed top-4 -full h-screen">
       <Image
-        src={"/landing_bg.gif"}
+        src={"/Landing_bg2.gif"}
         alt="Hero"
         height={1000}
         width={1000}
-        className="-z-10 fixed top-4 -full h-screen"
+        objectFit="contain"
+        unoptimized
+        className="-z-10 fixed top-4 h-screen"
       />
     </div>
   );
